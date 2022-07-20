e7301c037c6261155fb101ddaf5304994d0eba1c
chris <cdresher2010@gmail.com>
2022-07-20 00:39:00 +0000

    upgraded insertion to handle any key value pair that could be added

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,9 +1,20 @@
-# hashtable.py
+from hashtable.hashtable_utils import BLANK
 
 class HashTable:
     def __init__(self, capacity: int):
         # fill a list of some length N and give it default values
-        self.values: list[None] = capacity * [None]
+        self.values: list[BLANK] = capacity * [BLANK]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.values)
+
+    def __setitem__(self, key, value):
+        '''
+        You turn an arbitrary key into a numeric hash
+        value and use the modulo operator to constrain the
+        resulting index within the available address
+        space. Great! Your test report lights up green
+        again.
+        '''
+        index: int = hash(key) % len(self)
+        self.values[index] = value
--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -1,4 +1,6 @@
+import pytest
 from hashtable.hashtable import HashTable
+from hashtable.hashtable_utils import BLANK
 
 def test_should_create_hashtable():
     assert HashTable(capacity=100) is not None
@@ -7,4 +9,30 @@ def test_size_attr():
     assert len(HashTable(capacity=100)) == 100
 
 def test_should_create_empty_value_slots():
-    assert HashTable(capacity=3).values == [None, None, None]
+    assert HashTable(capacity=3).values == [BLANK, BLANK, BLANK]
+
+def test_should_insert_key_value_pairs():
+    hash_table = HashTable(capacity=100)
+
+    hash_table["hola"] = "hello"
+    hash_table[98.6] = 37
+    hash_table[False] = True
+
+    assert "hello" in hash_table.values
+    assert 37 in hash_table.values
+    assert True in hash_table.values
+
+    assert len(hash_table) == 100
+
+def test_should_not_contain_none_value_when_created():
+    assert None not in HashTable(capacity=100).values
+
+def test_should_insert_none_value():
+    hash_table = HashTable(capacity=100)
+    hash_table["key"] = None
+    assert None in hash_table.values
+
+@pytest.mark.skip
+# skip decorator lets you pre implement tests
+def test_should_not_shrink_when_removing_elements():
+    pass
