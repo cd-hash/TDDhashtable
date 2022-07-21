ddc5fd56ba00356db474cf71d05818cc41cbd92a
chris <cdresher2010@gmail.com>
2022-07-21 00:34:30 +0000

    finished implementing basic hash table functionality

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -8,7 +8,7 @@ class HashTable:
     def __len__(self) -> int:
         return len(self.values)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key, value): # this implements the dict[key] = value
         '''
         You turn an arbitrary key into a numeric hash
         value and use the modulo operator to constrain the
@@ -16,18 +16,18 @@ class HashTable:
         space. Great! Your test report lights up green
         again.
         '''
-        index: int = hash(key) % len(self)
+        index: int = self.__index(key)
         self.values[index] = value
 
-    def __getitem__(self, key):
-        index: int = hash(key) % len(self)
+    def __getitem__(self, key): # this implements the dict[key] returns value
+        index: int = self.__index(key)
         value = self.values[index]
         if value is not BLANK:
             return value
         else:
             raise KeyError(key)
 
-    def __contains__(self, key):
+    def __contains__(self, key): # this lets us use the 'in' operator
         try:
             self[key]
         except KeyError:
@@ -35,4 +35,19 @@ class HashTable:
         else:
             return True
 
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __delitem__(self, key): # this lets us use the del operator
+        if key in self:
+            self[key] = BLANK
+        else:
+            raise KeyError(key)
+
+    def __index(self, key):
+        return hash(key) % len(self)
+        
         
--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -2,6 +2,14 @@ import pytest
 from hashtable.hashtable import HashTable
 from hashtable.hashtable_utils import BLANK
 
+@pytest.fixture
+def hash_table():
+    sample_data = HashTable(capacity=100)
+    sample_data["hola"] = "hello"
+    sample_data[98.6] = 37
+    sample_data[False] = True
+    return sample_data
+
 def test_should_create_hashtable():
     assert HashTable(capacity=100) is not None
 
@@ -32,13 +40,15 @@ def test_should_insert_none_value():
     hash_table["key"] = None
     assert None in hash_table.values
 
-@pytest.fixture
-def hash_table():
-    sample_data = HashTable(capacity=100)
-    sample_data["hola"] = "hello"
-    sample_data[98.6] = 37
-    sample_data[False] = True
-    return sample_data
+def test_should_update_value(hash_table):
+    assert hash_table["hola"] == "hello"
+
+    hash_table["hola"] = "hallo"
+
+    assert hash_table["hola"] == "hallo"
+    assert hash_table[98.6] == 37
+    assert hash_table[False] is True
+    assert len(hash_table) == 100
 
 def test_should_find_value_by_key(hash_table):
     assert hash_table["hola"] == "hello"
@@ -57,6 +67,39 @@ def test_should_find_key(hash_table):
 def test_should_not_find_key(hash_table):
     assert "missing_key" not in hash_table
 
+def test_should_get_value(hash_table):
+    assert hash_table.get("hola") == "hello"
+
+def test_should_get_none_when_missing_key(hash_table):
+    assert hash_table.get("missing_key") is None
+
+def test_should_get_default_value_when_missing_key(hash_table):
+    assert hash_table.get("missing_key", "default") == "default"
+
+def test_should_get_value_with_default(hash_table):
+    assert hash_table.get("hola", "default") == "hello"
+
+def test_should_delete_key_value_pair(hash_table):
+    assert "hola" in hash_table
+    assert "hello" in hash_table.values
+    assert len(hash_table) == 100
+
+    del hash_table["hola"]
+
+    assert "hola" not in hash_table
+    assert "hello" not in hash_table.values
+    assert len(hash_table) == 100
+
+def test_should_raise_key_error_when_deleting(hash_table):
+    with pytest.raises(KeyError) as exception_info:
+        del hash_table["missing_key"]
+    assert exception_info.value.args[0] == "missing_key"
+
+def test_should_return_pairs(hash_table):
+    assert ("hola", "hello") in hash_table.pairs
+    assert (98.6, 37) in hash_table.pairs
+    assert (False, True) in hash_table.pairs
+    
 @pytest.mark.skip
 # skip decorator lets you pre implement tests
 def test_should_not_shrink_when_removing_elements():
